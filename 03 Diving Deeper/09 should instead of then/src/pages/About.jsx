9f8e25b3fdcea68d84c8360f56b1682fe1823225
Andrei Mateescu <andrei.radu.mateescu@gmail.com>
2023-03-23 13:30:27 +0200

    Added cypress projects

---
+++ 03 Diving Deeper/09 should instead of then/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import ContactForm from '../components/ContactForm';
+
+function AboutPage() {
+  return (
+    <>
+      <header className="center">
+        <h1>About Us</h1>
+        <p>
+          We are a small team of developers who are passionate about testing. We
+          have created this demo to help you learn how to use Cypress.
+        </p>
+      </header>
+      <ContactForm />
+    </>
+  );
+}
+
+export default AboutPage;
